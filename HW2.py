5120926f54de15219ddfc85e0d272fc5f82a5ab5
ajreese1 <ajreese1@crimson.ua.edu>
2023-02-02 15:21:42 -0600

    put object and movement class in separate file

--- HW2.py
+++ HW2.py
@@ -1,28 +1,7 @@
 from tkinter import *
 from queue import Queue
 from threading import Thread, Lock
-
-
-
-class Object:
-    def __init__(self,num):
-        self.shape = None
-        self.text = None
-        self.lineToParent = None
-        self.x = -1
-        self.y = -1
-        self.aniQueue = Queue()
-        self.moveQueue = Queue()
-        self.userNum = num
-
-class Movement:
-    def __init__(self,x,y,step=0,args=[],newLine=[],newObject=[]):
-        self.x = x
-        self.y = y
-        self.step = step
-        self.args = args
-        self.newLine = newLine
-        self.newObject = newObject
+from animation import *
     
 ###############################################################################################
 # A thread that produces data
